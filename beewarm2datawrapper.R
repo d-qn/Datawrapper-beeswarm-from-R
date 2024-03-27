74265e3826a2ac6930973ac8ca8e9247e15efc31
Duc Quang Nguyen <ducquang.nguyen@gmail.com>
2024-03-27 20:17:09 +0100

    Init

---
+++ beewarm2datawrapper.R
@@ -0,0 +1,54 @@
+library(beeswarm)
+library(tidyverse)
+library(DatawRappr)
+
+### 1. Load data
+
+# These are real data from Switzerland's federal ballot 
+# on the 3rd of March 2024 regarding the 13th pension, results by municipality
+ddd <- read_csv("https://gist.githubusercontent.com/d-qn/5cff401873518297fb029b3e3fe1b836/raw/9e930fa5739215eaea969fd72fb231e3461b9318/bs_13rente_muni.csv")
+
+
+### 2. Compute/plot the beeswarwm
+
+# Beeswarm of the % yes vote `qValue` by language region `value`
+# There is a hack here to assign the id of the data `qId` to the color, 
+# this will allow to join the computed beeswarm coordindates to the original data
+# In addition with getting the beeswarm coordinates, this will plot the beeswarm
+# Adjust the "cex" parameter to change the size of the points
+
+df_bees <- beeswarm(
+  qValue ~ value,
+  data = ddd ,
+  method = "compactswarm",
+  pwcol = qId,
+  cex =.4,
+  pch = 16, vertical = F
+)
+
+rownames(df_bees) <- NULL
+
+### 3. Join the computed beeswarm coordinates to the original data
+
+dfb <- left_join(
+  df_bees %>% select(x,y, x.orig, y.orig, col),
+  ddd %>% 
+    mutate(value = as.character(value)), 
+  by = c("col" = "qId")
+) 
+
+
+### 4. Optional - Send the data to Datawrapper via its API or manually upload it
+dfb %>% 
+  dw_data_to_chart("74ZFo", api_key = XX) # replace XX with your API key and datawrapper chart ID
+
+# or
+dfb %>% write_csv("beeswarm_data4datwwrapper.csv")
+
+
+### 5. In datawrapper 
+# (a. Upload the data)
+# b. Create a scatter plot
+# c. Assign the y column to the horizontal axis, x column to the vertical axis
+# d. Tweak & enjoy
+
